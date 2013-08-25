ffb53b240be29d33eaf7b13e451bafcb0bbe42c7
George Hines <george.hines@gmail.com>
2013-08-25 15:16:18 -0700

    Added ingredients table to schema

--- schema.sql
+++ schema.sql
@@ -7,3 +7,13 @@ create table dish (
   cost text,
   servings text
 );
+
+drop table if exists ingredients;
+create table ingredients (
+  id integer primary key autoincrement,
+  name text not null,
+  cost text,
+  accessibility text,
+  quantity text,
+  unit text
+);
